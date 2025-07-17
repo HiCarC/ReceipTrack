b626f3ed97a5ba157ef8df55550b914c62557624
HiCarC <carlescerqueda@gmail.com>
2025-07-18 01:55:11 +0200

    TestGroupLink V2.2

--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,24 @@ import './index.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import JoinGroupPage from './components/JoinGroupPage';
 import GroupExpensesPageLoader from './components/GroupExpensesPageLoader';
+import { AuthProvider } from './contexts/AuthContext';
+import { LoadingProvider } from './contexts/LoadingContext';
+import { GroupProvider } from './contexts/GroupContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/join/:groupId" element={<JoinGroupPage />} />
-        <Route path="/group/:groupId" element={<GroupExpensesPageLoader />} />
-        <Route path="/*" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <GroupProvider>
+      <AuthProvider>
+        <LoadingProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/join/:groupId" element={<JoinGroupPage />} />
+              <Route path="/group/:groupId" element={<GroupExpensesPageLoader />} />
+              <Route path="/*" element={<App />} />
+            </Routes>
+          </BrowserRouter>
+        </LoadingProvider>
+      </AuthProvider>
+    </GroupProvider>
   </React.StrictMode>
 ) 
